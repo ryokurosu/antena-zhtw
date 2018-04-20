3032fc459d68fe4c3cde9e577ef5e0bee9020fc4
Ryo Kurosu <knowrop1208@gmail.com>
2018-04-20 12:00:15 +0900

    a

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -17,11 +17,11 @@
     </div>
     <div class="block">
       <p><center>
-              <a href="http://bit.ly/2qB2KIe" rel="nofollow" target="_blank">[PR]【最新版】病院にいる9割の医者が知らないアトピー完治のコツを公開！？</a>
-            </center></p>
-            <p><center>
-              <a href="https://uranai-cafe.jp/animal/" rel="nofollow" target="_blank">[PR]動物キャラ占い(無料)で、あなたの性格・恋愛傾向・毎日の運勢・今後の人生の運気がわかります。</a>
-            </center></p>
+        <a href="http://bit.ly/2qB2KIe" rel="nofollow" target="_blank">[PR]【最新版】病院にいる9割の医者が知らないアトピー完治のコツを公開！？</a>
+      </center></p>
+      <p><center>
+        <a href="https://uranai-cafe.jp/animal/" rel="nofollow" target="_blank">[PR]動物キャラ占い(無料)で、あなたの性格・恋愛傾向・毎日の運勢・今後の人生の運気がわかります。</a>
+      </center></p>
     </div>
     <div class="meta-wrap">
       <p>{{$detail->description}}</p>
@@ -41,20 +41,20 @@
           <li class="list-group-item">
             <p><a href="http://bit.ly/2qB2KIe" rel="nofollow" target="_blank">{{'@kei'}}</a></p>
             <p>このサイト参考にしたら、アトピーが治りました。今まで夜は痒くて痒くて仕方なかったりしたのに助かった。。。<a style="text-decoration: underline;" href="http://bit.ly/2qB2KIe" rel="nofollow" target="_blank">http://bit.ly/2qB2KIe</p>
-          </li>
-          <li class="list-group-item">
-            <p><a href="https://masakuraudo2.com/archives/2051" rel="nofollow" target="_blank">{{'@mama_nerse'}}</a></p>
-            <p><a style="text-decoration: underline;" href="https://masakuraudo2.com/archives/2051" rel="nofollow" target="_blank">看護師・介護士のセクハラ被害の実態</p>
-          </li>
-          @foreach($detail->twitters as $tweet)
-          <li class="list-group-item">
-            <p><a href="{{$tweet->userLink()}}" rel="nofollow" target="_blank">{{'@'}}{{$tweet->user_id}}</a></p>
-            <p>{{$tweet->text}}</p>
-            <span class="time"><a href="{{$tweet->tweetLink()}}" rel="nofollow" target="_blank">{{$tweet->updated_at->format('Y/n/j H:i:s')}}</a></span>
-          </li>
-          @endforeach
-        </ul>
-      </div>
+            </li>
+            <li class="list-group-item">
+              <p><a href="https://masakuraudo2.com/archives/2051" rel="nofollow" target="_blank">{{'@mama_nerse'}}</a></p>
+              <p><a style="text-decoration: underline;" href="https://masakuraudo2.com/archives/2051" rel="nofollow" target="_blank">看護師・介護士のセクハラ被害の実態</p>
+              </li>
+              @foreach($detail->twitters as $tweet)
+              <li class="list-group-item">
+                <p><a href="{{$tweet->userLink()}}" rel="nofollow" target="_blank">{{'@'}}{{$tweet->user_id}}</a></p>
+                <p>{{$tweet->text}}</p>
+                <span class="time"><a href="{{$tweet->tweetLink()}}" rel="nofollow" target="_blank">{{$tweet->updated_at->format('Y/n/j H:i:s')}}</a></span>
+              </li>
+              @endforeach
+            </ul>
+          </div>
 
           <div class="sns-area">
             <ul class="social-button-syncer">
@@ -91,50 +91,61 @@
             <p><center><a href="{{$detail->url}}" class="btn btn-primary" rel="nofollow" target="_blank">記事を読む</a></center></p>
             <p><center><a href="https://binary-option-kouryaku.com/highlowstart/" rel="nofollow" target="_blank">[PR]【今なら5000円キャッシュバック】バイナリーオプションの始め方／口座開設→入金→取引まで実例解説</a></center></p>
           </div>
-            <div class="block">
-        <a class="ad" href="https://px.a8.net/svt/ejp?a8mat=2ZEUCM+6NCD1U+358E+61JSH" target="_blank" rel="nofollow">
-          <img border="0" width="300" height="250" alt="" src="https://www21.a8.net/svt/bgt?aid=180411430402&wid=005&eno=01&mid=s00000014675001015000&mc=1"></a>
+          <div class="block">
+            <a class="ad" href="https://px.a8.net/svt/ejp?a8mat=2ZEUCM+6NCD1U+358E+61JSH" target="_blank" rel="nofollow">
+              <img border="0" width="300" height="250" alt="" src="https://www21.a8.net/svt/bgt?aid=180411430402&wid=005&eno=01&mid=s00000014675001015000&mc=1"></a>
 
-          <a class="ad" href="https://px.a8.net/svt/ejp?a8mat=2ZEUCM+6YNLJM+1ORI+U13R5" target="_blank" rel="nofollow">
-            <img border="0" width="336" height="280" alt="" src="https://www28.a8.net/svt/bgt?aid=180411430421&wid=005&eno=01&mid=s00000007875005044000&mc=1"></a>
-          </div>
-          <ul class="list-group noback">
-            @foreach($articles as $article)
-            <li class="list-group-item">
-              <div class="padding-wrap">
-                <div class="col-xs-3 thumbnail">
-                  <a class="thumbnail-link" href="{{$article->path()}}">
-                    <img src="{{$article->imagePath()}}" alt="{{$article->title}}">
+              <a class="ad" href="https://px.a8.net/svt/ejp?a8mat=2ZEUCM+6YNLJM+1ORI+U13R5" target="_blank" rel="nofollow">
+                <img border="0" width="336" height="280" alt="" src="https://www28.a8.net/svt/bgt?aid=180411430421&wid=005&eno=01&mid=s00000007875005044000&mc=1"></a>
+              </div>
+
+              <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+              <ins class="adsbygoogle"
+              style="display:block"
+              data-ad-format="autorelaxed"
+              data-ad-client="ca-pub-5105681373982866"
+              data-ad-slot="4907655754"></ins>
+              <script>
+               (adsbygoogle = window.adsbygoogle || []).push({});
+             </script>
+             
+             <ul class="list-group noback">
+              @foreach($articles as $article)
+              <li class="list-group-item">
+                <div class="padding-wrap">
+                  <div class="col-xs-3 thumbnail">
+                    <a class="thumbnail-link" href="{{$article->path()}}">
+                      <img src="{{$article->imagePath()}}" alt="{{$article->title}}">
+                    </a>
+                  </div>
+                  <div class="col-xs-9 title">
+                    <a class="title-link" href="{{$article->path()}}">
+                     {{$article->title}}
+                   </a>
+                   <a class="description-link">
+                    @php
+                    echo mb_strimwidth($article->description, 0, 120, '', 'utf8');
+                    @endphp
                   </a>
                 </div>
-                <div class="col-xs-9 title">
-                  <a class="title-link" href="{{$article->path()}}">
-                   {{$article->title}}
-                 </a>
-                 <a class="description-link">
-                  @php
-                  echo mb_strimwidth($article->description, 0, 120, '', 'utf8');
-                  @endphp
-                </a>
+                <div class="clear"></div>
+                <div class="col-xs-12 cat">
+                  <span class="cat-item">
+                    {{$article->word->text}}
+                  </span>
+                  <span class="cat-domain">
+                    @php
+                    echo parse_url($article->url, PHP_URL_HOST);
+                    @endphp
+                  </span>
+                  <a href="{{$article->path()}}" class="link-btn">サイトへ</a>
+                </div>
+                <span class="view">{{$article->view}} view</span>
               </div>
-              <div class="clear"></div>
-              <div class="col-xs-12 cat">
-                <span class="cat-item">
-                  {{$article->word->text}}
-                </span>
-                <span class="cat-domain">
-                  @php
-                  echo parse_url($article->url, PHP_URL_HOST);
-                  @endphp
-                </span>
-                <a href="{{$article->path()}}" class="link-btn">サイトへ</a>
-              </div>
-              <span class="view">{{$article->view}} view</span>
-            </div>
-          </li>
+            </li>
 
-          @endforeach
-        </ul>
+            @endforeach
+          </ul>
+        </div>
       </div>
-    </div>
-    @endsection
+      @endsection
