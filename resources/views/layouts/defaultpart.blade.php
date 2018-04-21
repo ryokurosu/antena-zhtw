2b9581f4347f3981f5e8354af2ea854dd5ca6725
Ryo Kurosu <knowrop1208@gmail.com>
2018-04-21 09:45:37 +0900

    a

--- resources/views/layouts/defaultpart.blade.php
+++ resources/views/layouts/defaultpart.blade.php
@@ -1,7 +1,11 @@
 @section('link-ad')
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <!-- リンク広告 -->
-<ins class="adsbygoogle" style="display: block;" data-ad-client="ca-pub-5105681373982866" data-ad-slot="4133950625" data-ad-format="link"></ins>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-5105681373982866"
+     data-ad-slot="4133950625"
+     data-ad-format="link"></ins>
 <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
