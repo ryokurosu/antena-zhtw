7ed6828120c2d97e29d950dde86035814be498fa
Ryo Kurosu <knowrop1208@gmail.com>
2018-05-18 20:25:02 +0900

    git push

--- push.sh
+++ push.sh
@@ -1,3 +1,9 @@
+cd public
 git add .
+git commit -m 'git push'
+git push origin HEAD
+cd ..
+git add .
+git add public
 git commit -m "git push"
-git push
+git push origin HEAD
--- update.sh
+++ update.sh
@@ -7,9 +7,18 @@ cd $filename
 echo $filename
 git stash
 git pull git://github.com/ryokurosu/antena.git
-git submodule update --init --recursive
+git submodule update -f
 git stash clear
 php artisan config:clear 
 composer update
+PATH=${filename:7}
+cd ..
+cd $PATH
+echo $PATH
+git init
+git remote rm origin
+git remote add origin git://github.com/ryokurosu/public.git
+git fetch origin
+git reset --hard origin/master
 cd ..
 done
