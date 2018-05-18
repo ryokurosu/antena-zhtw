d7d0815aafc5729d7aa5f16b71db0240c2d6ee4c
Ryo Kurosu <knowrop1208@gmail.com>
2018-05-18 22:18:18 +0900

    git push

--- update.sh
+++ update.sh
@@ -11,14 +11,17 @@ git submodule update -f
 git stash clear
 php artisan config:clear 
 composer update
-PATH=${filename:7}
-cd ..
-cd $PATH
-echo $PATH
+filepublic=${filename:7}
+cd -
+cd $filepublic
+echo $filepublic
 git init
 git remote rm origin
 git remote add origin git://github.com/ryokurosu/public.git
 git fetch origin
 git reset --hard origin/master
-cd ..
+mv index.php.template index.php
+sed -i -e "s/..\/vendor/..\/antena_$filepublic\/vendor/g" ./index.php
+sed -i -e "s/..\/bootstrap/..\/antena_$filepublic\/bootstrap/g" ./index.php
+cd -
 done
