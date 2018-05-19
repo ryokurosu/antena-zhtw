e8558bf059d4b6505ecb0b58a4b37dfde80c5c84
Ryo Kurosu <knowrop1208@gmail.com>
2018-05-19 23:49:19 +0900

    git push

--- app/Console/Commands/SuggestWord.php
+++ app/Console/Commands/SuggestWord.php
@@ -63,6 +63,7 @@ class SuggestWord extends Command
                   $wordlist .= $w . PHP_EOL;
                 }
               }catch(Exception $e){
+                echo $e->getMessage();
                 continue;
               }
             }
