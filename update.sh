8e1e59fdfae1758f869a2ec83a77dcef87dc933a
Ryo Kurosu <knowrop1208@gmail.com>
2018-05-20 03:57:24 +0900

    git push

--- update.sh
+++ update.sh
@@ -1,20 +1,22 @@
-
 cd `dirname $0`
 for filename in `find antena_* -maxdepth 0 -type d`
 
 do
 cd $filename
 echo $filename
+git init
+git remote rm origin
+git remote add origin git://github.com/ryokurosu/antena-zhtw.git
 git fetch origin
 git reset --hard origin/master
 git submodule init
 git submodule update -f
-git stash clear
 php artisan config:clear 
 composer update
 filepublic=${filename:7}
 cd -
 mv $filename/schedule.sh schedule.sh
+mv $filename/update.sh update.sh
 cd $filepublic
 echo $filepublic
 git init
@@ -27,3 +29,6 @@ sed -i -e "s/..\/vendor/..\/antena_$filepublic\/vendor/g" ./index.php
 sed -i -e "s/..\/bootstrap/..\/antena_$filepublic\/bootstrap/g" ./index.php
 cd -
 done
+
+
+
