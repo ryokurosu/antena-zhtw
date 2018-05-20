5555c295076796020ec49317579105872af8d220
Ryo Kurosu <knowrop1208@gmail.com>
2018-05-20 12:14:50 +0900

    git push

--- push.sh
+++ push.sh
@@ -1,9 +1,9 @@
 cd public
 git add .
 git commit -m 'git push'
-git push -f
+git push https://github.com/ryokurosu/public.git
 cd ..
 git add .
 git add public
 git commit -m "git push"
-git push -f
+git push https://github.com/ryokurosu/antena-zhtw.git
