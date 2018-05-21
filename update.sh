f4054ee69f6141c96147ed078c97cfe3940286d0
Ryo Kurosu <knowrop1208@gmail.com>
2018-05-21 21:14:43 +0900

    git push

--- update.sh
+++ update.sh
@@ -4,14 +4,14 @@ for filename in `find antena_* -maxdepth 0 -type d`
 do
 cd $filename
 echo $filename
-git init
-git remote rm origin
-git remote add origin git://github.com/ryokurosu/antena-zhtw.git
-git fetch origin
-git reset --hard origin/master
-git submodule init
-git submodule sync
-git submodule foreach "(git checkout master; git pull)"
+~/opt/bin/git init
+~/opt/bin/git remote rm origin
+~/opt/bin/git remote add origin git://github.com/ryokurosu/antena-zhtw.git
+~/opt/bin/git fetch origin
+~/opt/bin/git reset --hard origin/master
+~/opt/bin/git submodule init
+~/opt/bin/git submodule sync
+~/opt/bin/git submodule foreach "(git checkout master; git pull)"
 php artisan config:clear 
 filepublic=${filename:7}
 cd -
@@ -19,11 +19,11 @@ mv $filename/schedule.sh schedule.sh
 mv $filename/update.sh update.sh
 cd $filepublic
 echo $filepublic
-git init
-git remote rm origin
-git remote add origin git://github.com/ryokurosu/public.git
-git fetch origin
-git reset --hard origin/master
+~/opt/bin/git init
+~/opt/bin/git remote rm origin
+~/opt/bin/git remote add origin git://github.com/ryokurosu/public.git
+~/opt/bin/git fetch origin
+~/opt/bin/git reset --hard origin/master
 mv index.php.template index.php
 sed -i -e "s/\.\.\/vendor\/autoload.php/\.\.\/antena_$filepublic\/vendor\/autoload.php/g" ./index.php
 sed -i -e "s/\.\.\/bootstrap\/app.php/\.\.\/antena_$filepublic\/bootstrap\/app.php/g" ./index.php
